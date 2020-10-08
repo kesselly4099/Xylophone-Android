415235622e727492c14eccced663e0575760ce2c
kesselly4099 <kkesselly30@gmail.com>
2020-10-08 14:25:04 -0700

    Challenge complete

--- app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private final float NORMAL_PLAY_RATE = 1.0f;
 
     // TODO: Add member variables here
+    private SoundPool msound;
     private int mCSoundId;
     private int mDSoundId;
     private int mESoundId;
@@ -31,15 +32,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // TODO: Create a new SoundPool
-
+msound = new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS,AudioManager.STREAM_MUSIC,1);
 
         // TODO: Load and get the IDs to identify the sounds
-
-
+mCSoundId = msound.load(getApplicationContext(),R.raw.note1_c,0);
+mDSoundId = msound.load(getApplicationContext(),R.raw.note2_d,0);
+mESoundId = msound.load(getApplicationContext(),R.raw.note3_e,0);
+mFSoundId = msound.load(getApplicationContext(),R.raw.note4_f,0);
+mGSoundId = msound.load(getApplicationContext(),R.raw.note5_g,0);
+mASoundId = msound.load(getApplicationContext(),R.raw.note6_a,0);
+mBSoundId = msound.load(getApplicationContext(),R.raw.note7_b,0);
     }
 
     // TODO: Add the play methods triggered by the buttons
 
-
-
+public void Playc(View v){
+        msound.play(mCSoundId,1.0f,1.0f,0,0,1.0f);
+}
+    public void Playd(View v){
+        msound.play(mDSoundId,1.0f,1.0f,0,0,1.0f);
+    }
+    public void Playe(View v){
+        msound.play(mESoundId,1.0f,1.0f,0,0,1.0f);
+    }
+    public void Playf(View v){
+        msound.play(mFSoundId,1.0f,1.0f,0,0,1.0f);
+    }
+    public void Playg(View v){
+        msound.play(mGSoundId,1.0f,1.0f,0,0,1.0f);
+    }
+    public void Playa(View v){
+        msound.play(mASoundId,1.0f,1.0f,0,0,1.0f);
+    }
+    public void Playb(View v){
+        msound.play(mBSoundId,1.0f,1.0f,0,0,1.0f);
+    }
 }
